52d527611e6d7dcd0f2407bca26ec806552b67cd
Nelson <nelsonalejandroramosrivera@gmail.com>
2021-11-19 11:47:04 -0300

    change array-set in second value tree

--- index.js
+++ index.js
@@ -4,12 +4,12 @@ function create(array) {
         for (var i = 0; i < value.length; i++) {
             if (tree.has(i)) {
                 if (tree.get(i).has(value.substring(0, i + 1))) {
-                    value.length == i + 1 ? tree.get(i).get(value.substring(0, i + 1)).push(index) : null
+                    value.length == i + 1 ? tree.get(i).get(value.substring(0, i + 1)).add(index) : null
                 } else {
-                    tree.get(i).set(value.substring(0, i + 1), value.length == i + 1 ? [index] : [])
+                    tree.get(i).set(value.substring(0, i + 1), value.length == i + 1 ? new Set([index]) : new Set([]))
                 }
             } else {
-                tree.set(i, new Map([[value.substring(0, i + 1), value.length == i + 1 ? [index] : []]]))
+                tree.set(i, new Map([[value.substring(0, i + 1), value.length == i + 1 ? new Set([index]) : new Set()]]))
             }
         }
     })
